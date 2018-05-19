4dbd179bd953e9e4c622cfbf5a6ea2e3f53262f1
LeoArouca <leonardo.arouca@gmail.com>
2018-05-19 19:38:36 +0800

    Adds component for the git data

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,34 @@ var React = require('react');
 var PropTypes = require ('prop-types');
 var api = require('../utils/api');
 
+function RepoGrid (props) {
+  return (
+    <ul className='popular-list'>
+      { props.repos.map(function  (repo, index){
+        return(
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index+1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}/>
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+          )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
 function SelectLanguage (props){
   var languages = ['All','Javascript','Ruby','Java','CSS','Python'];
   return(
@@ -67,7 +95,7 @@ class Popular extends React.Component{
         <SelectLanguage
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage} />
-        {JSON.stringify(this.state.repos,null,2)}
+        <RepoGrid repos={this.state.repos} />
       </div>
     );
   }
